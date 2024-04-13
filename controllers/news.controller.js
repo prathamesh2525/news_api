b5e4c1cc25a7109dcbe676e8cb818fb2514ed727
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2024-04-13 11:54:21 +0530

    implement pagination in news controller

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -6,7 +6,21 @@ import NewsApiTransform from "../transform/newsApiTransform.js"
 
 class NewsController {
   static async index(req, res) {
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 1
+
+    if (page <= 0) {
+      page = 1
+    }
+    if (limit <= 0 || limit > 100) {
+      limit = 10
+    }
+
+    const skip = (page - 1) * limit
+
     const news = await prisma.news.findMany({
+      take: limit,
+      skip: skip,
       include: {
         user: {
           select: {
@@ -18,7 +32,18 @@ class NewsController {
       },
     })
     const newsTransform = news?.map((item) => NewsApiTransform.transform(item))
-    res.json(newsTransform)
+
+    const totalNews = await prisma.news.count()
+    const totalPages = Math.ceil(totalNews / limit)
+
+    res.json({
+      news: newsTransform,
+      metadata: {
+        totalPages,
+        currentPage: page,
+        currentLimit: limit,
+      },
+    })
   }
 
   static async createNews(req, res) {
